93565de9459ba97773b5a607a397057c167bdb67
UnicornoftheSea99 <erhyu@smith.edu>
2019-02-09 19:50:57 -0500

    word count

--- dialect.js
+++ dialect.js
@@ -1,6 +1,20 @@
-function changeColor() {
-    document.getElementById("findBtn").style.color="red";
-}
 
-myParagraph = document.getElementById("findBtn");
-myParagraph.addEventListener("click", changeColor);
+$(document).ready(function() {
+    $("#word_count").on('keyup', function() {
+        var words = this.value.match(/\S+/g).length;
+        if (words > 200) {
+            // Split the string on first 200 words and rejoin on spaces
+            var trimmed = $(this).val().split(/\s+/, 200).join(" ");
+            // Add a space at the end to keep new typing making new words
+            $(this).val(trimmed + " ");
+        }
+        else {
+            $('#display_count').text(words);
+            $('#word_left').text(200-words);
+        }
+    });
+ }); 
+
+
+ find=document.getElementById("find");
+ //find.addEventListener("click",);
